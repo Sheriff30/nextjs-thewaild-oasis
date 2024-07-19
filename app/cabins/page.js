4766d36eca5e157f98f8451186f1fc796c320447
Sherif Fouad <130251496+Sheriff30@users.noreply.github.com>
2024-07-19 07:51:16 +0300

    initial commit �

---
+++ app/cabins/page.js
@@ -0,0 +1,18 @@
+async function page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default page;
